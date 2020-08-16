b18c8d635b26daf0f002f4251756bf3552e8a0bb
Zach Kusz <z.s.kusz@gmail.com>
2020-08-16 18:59:31 -0500

    Added environment. Handle requests to site.

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const poetry = require('./routes/poetry');
 
 fastify.register(poetry);
 
+const corsHeader = {
+  name: 'Access-Control-Allow-Origin',
+  value: process.env.PORT ? 'https://color-golf.netlify.app' : 'http://localhost:8080',
+};
+fastify.get('/', async (request, reply) => {
+  reply.header(corsHeader.name, corsHeader.value);
+  reply.send(`Hi! This is the url for the poetry reader api. Access the app here: ${corsHeader.value}`);
+});
+
 const start = async () => {
   const port = process.env.PORT || 3000;
   try {
